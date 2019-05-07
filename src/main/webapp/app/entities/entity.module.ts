d60df7eb01df46aa469e97f849d2119d41b22d7c
trebushkov <trebushkov@gmail.com>
2019-05-07 16:36:20 +0300

    Initial application generated by JHipster-5.8.2

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,26 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
-            {
-                path: 'incident',
-                loadChildren: './incident/incident.module#BlacklisthotelIncidentModule'
-            },
-            {
-                path: 'incident',
-                loadChildren: './incident/incident.module#BlacklisthotelIncidentModule'
-            },
-            {
-                path: 'incident',
-                loadChildren: './incident/incident.module#BlacklisthotelIncidentModule'
-            },
-            {
-                path: 'incident',
-                loadChildren: './incident/incident.module#BlacklisthotelIncidentModule'
-            },
-            {
-                path: 'incident',
-                loadChildren: './incident/incident.module#BlacklisthotelIncidentModule'
-            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
